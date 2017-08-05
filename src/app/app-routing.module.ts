11a2e8ef070da8287b5d7ef6fcd70fdba3a08867
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 22:43:20 +0800

    01~04 Route設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Route, Routes,  RouterModule} from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { AppComponent } from './app.component';
+import { fallbackRoute } from './shared/fallback-rout';
 
 const routes: Routes = [
-  { path: '', component: Page1Component},
+  { path: '', component: AppComponent},
   { path: 'Page1', component: Page1Component},
-  { path: 'Page2', component: Page2Component}
+  { path: 'Page2', component: Page2Component},
+  fallbackRoute
 ];
 
 @NgModule({
---
+++ src/app/shared/fallback-rout.ts
@@ -0,0 +1,2 @@
+import { Route } from '@angular/router';
+export const fallbackRoute: Route = { path: '**', redirectTo: '/', pathMatch: 'full'};
